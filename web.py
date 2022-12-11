65bc91a747af8bb25c4ba3e5029efebab26b61fe
FrancescoT <fratra93@gmail.com>
2022-12-11 20:34:56 +0000

    minor update

--- web.py
+++ web.py
@@ -22,5 +22,4 @@ for index, todo in enumerate(todos):
         st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo' )
-st.session_state
+              on_change=add_todo, key='new_todo')
